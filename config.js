7afe607bdf578ef7b1f28e45d47b88a4c2e3719d
wangenze <15041567431@126.com>
2023-09-22 13:19:54 +0800

    :tada: 初始化我的站点监测

---
+++ config.js
@@ -0,0 +1,37 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '我的站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2310914-12c9075133a94017d4f59f6a',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '我的主页',
+      url: 'https://www.wangez.site'
+    },
+    {
+      text: '我的笔记',
+      url: 'https://note.wangez.site'
+    },
+    {
+      text: 'github主页demo',
+      url: 'https://demo.nedwang.cn'
+    },
+    {
+      text: 'Api服务',
+      url: 'https://api.wangez.site'
+    },
+  ],
+};
